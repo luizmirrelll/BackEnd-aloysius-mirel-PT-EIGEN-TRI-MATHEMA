f7787c77b5537f59d2ef0a613c582aae2fed2015
aloisius.mirel <aloisius.mirel@redtech.co.id>
2024-06-07 15:48:29 +0700

    fix : penjelasan

--- contoh-pemakaian.ts
+++ contoh-pemakaian.ts
@@ -10,13 +10,13 @@ async function bootstrap() {
   console.log(await libraryService.checkMembers());
 
   try {
-    console.log(await libraryService.borrowBook('M001', 'JK-45'));
+    console.log(await libraryService.borrowBook('M001', 'JK-45')); // Anggota Angga meminjam Harry Potter
   } catch (error) {
     console.error(error.message);
   }
 
   try {
-    console.log(await libraryService.borrowBook('M002', 'JK-45'));
+    console.log(await libraryService.borrowBook('M002', 'JK-45')); // Anggota Ferry mencoba meminjam Harry Potter
   } catch (error) {
     console.error(error.message);
   }
@@ -25,7 +25,7 @@ async function bootstrap() {
   console.log(await libraryService.checkMembers());
 
   try {
-    console.log(await libraryService.returnBook('M001', 'JK-45', 8));
+    console.log(await libraryService.returnBook('M001', 'JK-45', 8)); // Anggota Angga mengembalikan Harry Potter setelah 8 hari
   } catch (error) {
     console.error(error.message);
   }
